b03cacbb2c742e4246cb1cb7da6f46dca1594713
laatu <laatuji@gmail.com>
2016-08-08 18:17:06 +0530

    commit try seems perfected

--- anujge/db/dbConfig.php
+++
@@ -1,1 +0,0 @@
-<?phpdefine ("DB_HOST", "localhost"); // set database hostdefine ("DB_USER", "root"); // set database userdefine ("DB_PASS",""); // set database passworddefine ("DB_NAME",""); // set database name$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");$db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");?>
