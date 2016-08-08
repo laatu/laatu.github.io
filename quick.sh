06cb749c7a8d649b47ff75149fc2abf53b52c021
laatu <laatuji@gmail.com>
2016-08-08 17:23:46 +0530

    commit try seems perfected

--- quick.sh
+++ quick.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 # for quick commit
-#
-#
-
-git add .
-echo "Give commit Name:"
-read $commit
-git commit -m '$commit'
-git push
-git status
+git add  . && git commit -m "commit try seems perfected" && git push && notify-send "done"
